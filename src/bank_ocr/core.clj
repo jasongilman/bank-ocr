4f4462c442cdf1366e4877895ae1f066bed5ab93
Jason Gilman <jason@element84.com>
2014-11-15 08:53:37 -0500

    Implemented basic parts of user story 1.

--- src/bank_ocr/core.clj
+++ src/bank_ocr/core.clj
@@ -1,4 +1,5 @@
 (ns bank-ocr.core
+  "TODO"
   (:require [clojure.string :as str]
             [clojure.set :as set]))
 
@@ -15,11 +16,11 @@
   of the subsequences with the same index.
   
   (transpose [[1 2 3]
-  [4 5 6]
-  [7 8 9]])
+              [4 5 6]
+              [7 8 9]])
   => [[1 4 7]
-  [2 5 8]
-  [3 6 9]]
+      [2 5 8]
+      [3 6 9]]
   
   Source: http://stackoverflow.com/questions/10347315/matrix-transposition-in-clojure"
   [m]
@@ -76,31 +77,66 @@
    [\| \_ \|]])
 
 (def char-matrix->digit
-  {one "1"
-   two "2"
-   three "3"
-   four "4"
-   five "5"
-   six "6"
-   seven "7"
-   eight "8"
-   nine "9"
-   zero "0"})
+  {one \1
+   two \2
+   three \3
+   four \4
+   five \5
+   six \6
+   seven \7
+   eight \8
+   nine \9
+   zero \0})
 
 (def digit->char-matrix
-  (set/invert-map char-matrix->digit))
+  (set/map-invert char-matrix->digit))
 
 (def example
   (str 
     "    _  _     _  _  _  _  _ \n" 
     "  | _| _||_||_ |_   ||_||_|\n"  
     "  ||_  _|  | _||_|  ||_| _|\n"
-    "                           \n"   ))
+    "\n"
+    "    _  _     _  _  _  _  _ \n" 
+    "  | _| _||_||_ |_   ||_||_|\n"  
+    "  ||_  _|  | _||_|  ||_| _|\n"))
 
-(->> (str/split example #"\n")
-     ;; Remove blank line
-     drop-last
-     (map (partial partition 3))
-     transpose
-     (map char-matrix->digit))
+
+(defn print-account-number
+  "Takes an account number string and 'prints' it to a string"
+  [account-num]
+  (let [entry (->> account-num
+                   ;; Convert each digit of the account number into the character matrix
+                   (map digit->char-matrix)
+                   ;; Transpose the sequence of matrices to make it easier to connect together each line of the
+                   ;; printed account number
+                   transpose
+                   ;; Connect together each line of the account number
+                   (map (partial apply concat))
+                   (map str/join)
+                   ;; Join together the lines of the entry
+                   (str/join "\n"))]
+    ;; Append a blank line at the end of an entry
+    (str entry "\n")))
+
+(defn parse-account-number
+  "Takes a printed account number entry representing digits as spaces, underscores, and pipes parses
+  it and returns the account number."
+  [entry]
+  ;; TODO this would be a good place to use transducers to avoid multiple lazy sequences
+  (->> entry
+       str/split-lines
+       ;; Remove blank line at the end. 
+       (take 3)
+       ;; Split each line into groups of 3 characters
+       (map (partial partition 3))
+       ;; Transpose the sequences of sequences to create a sequence 3 by 3 matrices. 
+       ;; Each 3x3 character matrix represents a single account number digit
+       transpose
+       ;; Convert each character matrix into a digit
+       (map char-matrix->digit)
+       ;; Join together the digits into an account number
+       str/join))
+
+
 
--- test/bank_ocr/test/core.clj
+++ test/bank_ocr/test/core.clj
@@ -1,61 +1,108 @@
 (ns bank-ocr.test.core
   (:require [clojure.test :refer :all]
+            [clojure.test.check.properties :refer [for-all]]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.string :as str]
             [bank-ocr.core :as b]))
 
-             
+(def digits
+  "Generator for digits"
+  (gen/elements "012345689"))
+
+(def account-numbers
+  "Generator for account number. Each account number is a string of 9 digits"
+  (gen/fmap str/join (gen/vector digits 9)))
+
+(defspec generate-and-parse-account-number-spec 1000
+  (for-all [account-num account-numbers]
+    (= account-num (b/parse-account-number (b/print-account-number account-num)))))
+
+(defn account-nums->entry-file-string
+  [account-nums]
+  (str (->> account-nums
+         (map b/print-account-number)
+         (str/join "\n"))
+       "\n"))
+
+(comment 
+  (println (account-nums->entry-file-string (vals user-story-1-examples)))
+  
+  (println (str "\n" (str/join (keys user-story-1-examples))))
+
+(defspec parse-account-file-spec 100
+  (for-all [account-nums (gen/vector account-numbers)]
+    (let [account-file ])))
+ )
+
 (def user-story-1-examples
   {(str " _  _  _  _  _  _  _  _  _ \n"
         "| || || || || || || || || |\n"
         "|_||_||_||_||_||_||_||_||_|\n"
-        "                           \n")
+        "\n")
    "000000000"
    (str "                           \n"
         "  |  |  |  |  |  |  |  |  |\n"
         "  |  |  |  |  |  |  |  |  |\n"
-        "                           \n")
+        "\n")
    "111111111"
    (str " _  _  _  _  _  _  _  _  _ \n"
         " _| _| _| _| _| _| _| _| _|\n"
         "|_ |_ |_ |_ |_ |_ |_ |_ |_ \n"
-        "                           \n")
+        "\n")
    "222222222"
    (str " _  _  _  _  _  _  _  _  _ \n"
         " _| _| _| _| _| _| _| _| _|\n"
         " _| _| _| _| _| _| _| _| _|\n"
-        "                           \n")
+        "\n")
    "333333333"
    (str "                           \n"
         "|_||_||_||_||_||_||_||_||_|\n"
         "  |  |  |  |  |  |  |  |  |\n"
-        "                           \n")
+        "\n")
    "444444444"
    (str " _  _  _  _  _  _  _  _  _ \n"
         "|_ |_ |_ |_ |_ |_ |_ |_ |_ \n"
         " _| _| _| _| _| _| _| _| _|\n"
-        "                           \n")
+        "\n")
    "555555555"
    (str " _  _  _  _  _  _  _  _  _ \n"
         "|_ |_ |_ |_ |_ |_ |_ |_ |_ \n"
         "|_||_||_||_||_||_||_||_||_|\n"
-        "                           \n")
+        "\n")
    "666666666"
    (str " _  _  _  _  _  _  _  _  _ \n"
         "  |  |  |  |  |  |  |  |  |\n"
         "  |  |  |  |  |  |  |  |  |\n"
-        "                           \n")
+        "\n")
    "777777777"
    (str " _  _  _  _  _  _  _  _  _ \n"
         "|_||_||_||_||_||_||_||_||_|\n"
         "|_||_||_||_||_||_||_||_||_|\n"
-        "                           \n")
+        "\n")
    "888888888"
    (str " _  _  _  _  _  _  _  _  _ \n"
         "|_||_||_||_||_||_||_||_||_|\n"
         " _| _| _| _| _| _| _| _| _|\n"
-        "                           \n")
+        "\n")
    "999999999"
    (str "    _  _     _  _  _  _  _ \n"
         "  | _| _||_||_ |_   ||_||_|\n"
         "  ||_  _|  | _||_|  ||_| _|\n"
-        "                           \n")
+        "\n")
    "123456789"})
+
+
+(deftest parse-user-story-1-examples-test
+  (doseq [[entry account-num] user-story-1-examples]
+    (is (= account-num (b/parse-account-number entry)))))
+
+(deftest parse-user-story-1-examples-file
+  ;; Make sure we generate a valid file
+  (is (= (str/join (keys user-story-1-examples)) 
+         (account-nums->entry-file-string (vals user-story-1-examples))))
+  
+  ;; 
+  
+  )
